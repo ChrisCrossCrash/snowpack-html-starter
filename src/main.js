76daa3bbfb8a3ec0ed05d6df539fc7c2b6be9307
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-08 18:29:10 +0300

    Set up default Three.js scene

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import 'modern-normalize'
 import './styles.scss'
+import * as THREE from 'three'
 
-document.body.append('TODO: make something')
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75, // Field of view
+  window.innerWidth / window.innerHeight, // Aspect ratio
+  0.1, // Near clipping plane
+  1000 // Far clipping plane
+)
+camera.position.z = 5
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+function animate() {
+  requestAnimationFrame(animate)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+}
+animate()
